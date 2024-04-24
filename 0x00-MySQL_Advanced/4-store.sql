d508935b0e249d972656510649c14d55b5506dc3
Meed <meedouazane@yahoo.com>
2024-04-24 20:50:00 +0000

    4s

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,5 +2,5 @@ CREATE TRIGGER items_order
 AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items
-SET quantity = OLD.quantity - NEW.number
-WHERE name = item_name;
+SET quantity = quantity - NEW.number
+WHERE name=NEW.item_name;
