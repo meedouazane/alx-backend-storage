bc2703a2a47589e6c6e4228e7c3f7ca6d79d9342
Meed <meedouazane@yahoo.com>
2024-04-25 12:26:33 +0000

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,11 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+DELIMITER //
 CREATE procedure ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     SELECT users.id, users.name, CAST(AVG(corrections.score) AS DECIMAL(10,0)) AS average_score 
     FROM corrections 
-    INNER JOIN users ON user_id = users.id 
-    WHERE users.id=user_id
+    INNER JOIN users ON corrections.user_id = users.id 
+    WHERE users.id = user_id
     GROUP BY users.id, users.name;
-END
+END //
+DELIMITER;
