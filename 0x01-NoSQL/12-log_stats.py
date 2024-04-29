2b30134c4826d04e76f8bf8b2315dcb61cc605d1
Meed <mohamedouazane@yahoo.com>
2024-04-29 12:25:45 -0700

    12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Log stats """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    result = logs_collection.find()
+    methods = {
+        'GET': 0,
+        'POST': 0,
+        'PUT': 0,
+        'DELETE': 0,
+        'PATCH': 0,
+    }
+    i = 0
+    for log in result:
+        i += 1
+        met = log.get('method')
+        if met in methods:
+            methods[met] += 1
+
+    print(f'{i} logs')
+    print('Methods:')
+    for method, number in methods.items():
+        print(f'\tmethod {method} : {number}')
+    status = 0
+    result2 = logs_collection.find()
+    for check in result2:
+        if check.get('path') == '/status':
+            status += 1
+    print(f'{status} status check')
