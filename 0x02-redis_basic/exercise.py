17a4631f02aecd074987dbb2ae7016dcc36ad817
Meed <mohamedouazane@yahoo.com>
2024-05-01 12:24:47 +0000

    0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ from typing import Union, Callable
 class Cache:
     """ Cache class """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """ init method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -21,26 +21,3 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
-    def get(self, key: str, fn: Callable = None):
-        """
-        get method that take a key string argument
-        and an optional Callable argument named fn
-        """
-        data = self._redis.get(key)
-        if fn:
-            return fn(data)
-        else:
-            return None
-
-    def get_str(self, data):
-        """
-        :return: data as string
-        """
-        return str(data)
-
-    def get_int(self, data):
-        """
-        :return: data as int
-        """
-        return int(data)
