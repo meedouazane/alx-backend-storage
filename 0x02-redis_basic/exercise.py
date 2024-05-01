af8d50edf5c6d254638adfa78a1b80b0f84bbc28
Meed <mohamedouazane@yahoo.com>
2024-05-01 12:35:57 +0000

    2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,13 +2,13 @@
 """ Writing strings to Redis"""
 import uuid
 import redis
-from typing import Union, Callable
+from typing import Union, Callable, Optional
 
 
 class Cache:
     """ Cache class """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """ init method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -21,3 +21,30 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> (
+            Union)[str, int, float, bytes]:
+        """
+        get method that take a key string argument
+        and an optional Callable argument named fn
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return data
+        if fn:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, data):
+        """
+        :return: data as string
+        """
+        return str(data)
+
+    def get_int(self, data):
+        """
+        :return: data as int
+        """
+        return int(data)
